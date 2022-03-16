9268ea93ae53665230c1b73ca402fc37ce753cbe
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-16 20:18:45 +0000

    New Maven project creation with JUnit5 dependency added as well

---
+++ src/test/java/com/techreturners/RomanNumeralsConverterTest.java
@@ -0,0 +1,12 @@
+package com.techreturners;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanNumeralsConverterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
