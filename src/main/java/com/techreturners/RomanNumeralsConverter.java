439f347e8a549b57e8465d9085fde136465c6864
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-17 03:23:42 +0000

    Refractored main method, added regex pattern match for user input, user can now also enter input multiple times unless choose to end programme

--- src/main/java/com/techreturners/RomanNumeralsConverter.java
+++ src/main/java/com/techreturners/RomanNumeralsConverter.java
@@ -5,16 +5,36 @@ import java.util.Scanner;
 
 public class RomanNumeralsConverter {
 
+    private static final RomanNumeralsConverter rnc = new RomanNumeralsConverter();
+
     public static void main(String[] args) {
-        RomanNumeralsConverter rnc = new RomanNumeralsConverter();
+       rnc.getUserInput();
+
+    }
+
+    public void getUserInput(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter roman numeral: ");
-        String romanNumeral = sc.nextLine();
-        int number = rnc.convertRomanNumerals(romanNumeral);
-        System.out.println("You converted the Roman Numeral " + romanNumeral + " to the number " + number);
+        String romanNumeral;
+        String pattern = "^(?=[IVXLCDM])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$";
+        while(true){
+            System.out.println("**** To quit programme enter q or 0 ****");
+            System.out.print("Please enter Roman Numeral: ");
+            romanNumeral = sc.nextLine();
+            if(!romanNumeral.toUpperCase().matches(pattern)){
+                rnc.errorMessageNonRomanNumeral(romanNumeral);
+            }
+            else if(romanNumeral.equalsIgnoreCase("q") || romanNumeral.equalsIgnoreCase("0")){
+                break;
+            }else{
+                int number = rnc.convertRomanNumerals(romanNumeral);
+                System.out.println("You converted the Roman Numeral " + romanNumeral.toUpperCase() + " to the number " + number);
+                System.out.println();
+            }
+        }
     }
 
     public HashMap<Character, Integer> mapOfRomanNumerals() {
+        //Store the fundamental roman numerals in a hashmap
         HashMap<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -26,7 +46,14 @@ public class RomanNumeralsConverter {
         return map;
     }
 
+    public void errorMessageNonRomanNumeral(String roman){
+        System.out.println("Please re-enter valid Roman Numerals ----> [IVXLCDM] and Numerals should not repeat > 3x");
+        System.out.println();
+    }
+
     public int convertRomanNumerals(String roman) {
+        //compare the first numeral with the next, if first is smaller --> subtract else add to value
+        roman = roman.toUpperCase();
         int value = 0;
         char last = roman.charAt(roman.length() - 1);
         for (int i = 0; i < roman.length() - 1; i++) {
