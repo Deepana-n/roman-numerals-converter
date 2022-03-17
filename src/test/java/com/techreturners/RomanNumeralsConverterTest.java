8298273484bf9e41b52d274d2313b74d1a137aff
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-17 02:11:20 +0000

    Junit test cases

--- src/test/java/com/techreturners/RomanNumeralsConverterTest.java
+++ src/test/java/com/techreturners/RomanNumeralsConverterTest.java
@@ -1,12 +1,42 @@
 package com.techreturners;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanNumeralsConverterTest {
 
+    private RomanNumeralsConverter rnc;
+
     @BeforeEach
-    void setUp() {
+    public void setup(){
+        rnc = new RomanNumeralsConverter();
     }
+
+    @Test
+    public void checkRomanNumeralI(){
+       assertEquals(1, rnc.convertRomanNumerals("I"));
+    }
+
+    @Test
+    public void checkRomanNumeralWhereSmallBeforeLargeIX(){
+        assertEquals(9, rnc.convertRomanNumerals("IX"));
+    }
+
+    @Test
+    public void checkRomanNumeralWhereLargeBeforeSmallXIII(){
+        assertEquals(13, rnc.convertRomanNumerals("XIII"));
+    }
+
+    @Test
+    public void checkRomanNumeralSingleD(){
+        assertEquals(500, rnc.convertRomanNumerals("D"));
+    }
+
+    @Test
+    public void checkRomanNumeralLargeMMMMDCCLXXXII(){
+        assertEquals(4782, rnc.convertRomanNumerals("MMMMDCCLXXXII"));
+    }
+
 }
